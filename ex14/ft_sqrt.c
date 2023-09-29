eee45afc7d1fdee933f0276d1be794d8c4864cad
Sara Perez-Mallaina De Aramburu <sarperez@c4r4s2.42urduliz.com>
2023-09-29 17:24:47 +0200

    Mejoro el ex14

--- ex14/ft_sqrt.c
+++ ex14/ft_sqrt.c
@@ -6,32 +6,39 @@
 /*   By: sarperez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 13:27:44 by sarperez          #+#    #+#             */
-/*   Updated: 2023/09/29 13:44:41 by sarperez         ###   ########.fr       */
+/*   Updated: 2023/09/29 17:21:40 by sarperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+//Empiezo suponiendo que el resultado es 1. Recalco que para <= 0 no. Epsilon para ver cuando me he acercado lo suficiente. Diff para ver la differencia. En el bucle hacemos valor absoluto. a la formula para hallarla. %f para sacar numeros decimales
 #include<unistd.h>
 #include<stdio.h>
 
-int	ft_sqrt(int nb);
-int	ft_sqrt(int nb)
+double	ft_sqrt(double nb);
+double	ft_sqrt(double nb)
 {
-	int a;
+	double a;
 
-	a = 0.01;
+	a = 1.0;
 
 	if(nb <= 0)
 		return(0);
 	else
 	{
-		while((a*a) != nb && nb > 0)
+		double epsilon;
+		double diff;
+
+		diff = a* a -nb;
+		epsilon	= 0.001;
+		while(diff > epsilon || diff < -epsilon)
 		{
-			a = a + 1;
-			return (a);
+			a = (a + nb / a)/ 2;
+			diff = a* a - nb;
 		}
+	return (a);
 	}
 }
 int main()
 {
-	printf("%d/n", ft_sqrt(4));
+	printf("%f/n", ft_sqrt(4));
+	return (0);
 }
